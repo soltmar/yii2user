1c9977e41c2f702d78108865d7348be9201b5e01
Mariusz Soltys <soltys.mariusz@yahoo.co.uk>
2015-10-25 22:35:29 +0000

    WIP

--- views/layouts/column2.php
+++ views/layouts/column2.php
@@ -1,29 +1,24 @@
 <?php
 
-    /* @var $this \yii\web\View */
-    use mariusz_soltys\yii2user\Module;
-    use mariusz_soltys\yii2user\UserAssets;
-    use yii\bootstrap\Nav;
-    use yii\widgets\Menu;
+/* @var $this \yii\web\View */
+use mariusz_soltys\yii2user\Module;
+use mariusz_soltys\yii2user\UserAssets;
+use yii\bootstrap\Nav;
+use yii\widgets\Menu;
 
 //UserAssets::register($this);
 
-    /* @var $content string */
+/* @var $content string */
 
-    $this->beginContent(Module::getInstance()->mainLayout); ?>
+$this->beginContent(Module::getInstance()->mainLayout); ?>
 
-    <div class="row">
-        <div class="col-md-10">
-            <?= $content ?>
-        </div>
-        <div class="col-md-2">
-            <?=
-                Nav::widget([
-                    'items' => Module::getInstance()->getMenu(),
-                    'options' => ['class' =>'nav-pills'], // set this to nav-tab to get tab-styled navigation
-                ]);
-            ?>
-        </div>
-    </div>
+<?=
+Nav::widget([
+    'items' => Module::getInstance()->getMenu(),
+    'options' => ['class' =>'nav-pills'], // set this to nav-tab to get tab-styled navigation
+]);
+?>
+
+<?= $content ?>
 
 <?php $this->endContent();
